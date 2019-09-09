525037d76f450e52ddd88bca4bba5988e573d9bb
reenesa <susannins@gmail.com>
2019-09-09 15:50:31 +0300

    version one

---
+++ week2partB.js
@@ -0,0 +1,18 @@
+//the issue is I want to change num to incrementing numbers
+var squareArray=[];
+var object2=[]
+// % sudo snap install vlc
+// function square(num){
+//     var number=num
+//     return num*2
+// }
+for(num=2; num<15; num++){
+    
+var object ={
+    num:num*2
+}
+object2.push(object)
+Object.key=+1;
+
+}
+console.log( object2)
